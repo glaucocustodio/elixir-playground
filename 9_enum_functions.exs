73d67d43f814f51d30fcc74412a2a394f08cac22
Glauco Custódio <glauco.custodio@gmail.com>
2015-01-08 23:55:25 +0000

    each function implementation

--- 9_enum_functions.exs
+++ 9_enum_functions.exs
@@ -6,10 +6,19 @@ defmodule MyEnum do
   def all?([head | tail], func) do
     func.(head) && all?(tail, func)
   end
+
+  # Invokes the given fun for each item in the collection. Returns :ok
+  def each([], _), do: :ok
+  def each([head | tail], func) do
+    func.(head)
+    each(tail, func)
+  end
 end
 
 # Should return true
 IO.puts MyEnum.all?([2,4,6], fn(x) -> rem(x, 2) == 0 end)
-
 # Should return false
-IO.puts MyEnum.all?([1, 2,4,6], fn(x) -> rem(x, 2) == 0 end)
+IO.puts MyEnum.all?([1,2,4,6], fn(x) -> rem(x, 2) == 0 end)
+
+# Should display 'hello\nworld'
+IO.inspect MyEnum.each(["hello", "world"], fn(x) -> IO.puts x end)
