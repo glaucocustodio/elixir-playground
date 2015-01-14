3db06d41c3e164d478966ee21efbf5fbe0916979
Glauco Custódio <glauco.custodio@gmail.com>
2015-01-14 22:04:29 +0000

    Filter function implemented

--- 9_enum_functions.exs
+++ 9_enum_functions.exs
@@ -13,6 +13,14 @@ defmodule MyEnum do
     func.(head)
     each(tail, func)
   end
+
+  def filter([], _, t), do: t
+  def filter([head | tail], func, t \\ []) do
+    if func.(head) do
+      t = t ++ [head]
+    end
+    filter(tail, func, t)
+  end
 end
 
 # Should return true
@@ -22,3 +30,6 @@ IO.puts MyEnum.all?([1,2,4,6], fn(x) -> rem(x, 2) == 0 end)
 
 # Should display 'hello\nworld'
 IO.inspect MyEnum.each(["hello", "world"], fn(x) -> IO.puts x end)
+
+# Filters the collection, returns only those elements for which fun returns true
+IO.inspect MyEnum.filter([1,2,3,4,5,6], fn(x) -> rem(x, 3) == 0 end)
